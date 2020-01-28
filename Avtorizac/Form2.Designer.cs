87b540a61fd80a709d38afcfa9795743c81590a2
Hnacstria <60339615+Hnacstria@users.noreply.github.com>
2020-01-28 09:02:30 +0600

    Forum3.0
    Design

--- Avtorizac/Form2.Designer.cs
+++ Avtorizac/Form2.Designer.cs
@@ -35,10 +35,10 @@
             // 
             // buttonOtpravka
             // 
-            this.buttonOtpravka.Location = new System.Drawing.Point(769, 562);
-            this.buttonOtpravka.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonOtpravka.Location = new System.Drawing.Point(577, 535);
+            this.buttonOtpravka.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.buttonOtpravka.Name = "buttonOtpravka";
-            this.buttonOtpravka.Size = new System.Drawing.Size(103, 35);
+            this.buttonOtpravka.Size = new System.Drawing.Size(77, 34);
             this.buttonOtpravka.TabIndex = 0;
             this.buttonOtpravka.Text = "Отправить";
             this.buttonOtpravka.UseVisualStyleBackColor = true;
@@ -47,34 +47,35 @@
             // listBoxForum
             // 
             this.listBoxForum.FormattingEnabled = true;
-            this.listBoxForum.ItemHeight = 21;
-            this.listBoxForum.Location = new System.Drawing.Point(9, 8);
-            this.listBoxForum.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxForum.ItemHeight = 20;
+            this.listBoxForum.Location = new System.Drawing.Point(7, 8);
+            this.listBoxForum.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.listBoxForum.Name = "listBoxForum";
-            this.listBoxForum.Size = new System.Drawing.Size(863, 550);
+            this.listBoxForum.Size = new System.Drawing.Size(648, 524);
             this.listBoxForum.TabIndex = 1;
             // 
             // tB
             // 
-            this.tB.Location = new System.Drawing.Point(9, 562);
+            this.tB.Location = new System.Drawing.Point(7, 535);
+            this.tB.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.tB.Multiline = true;
             this.tB.Name = "tB";
-            this.tB.Size = new System.Drawing.Size(749, 35);
+            this.tB.Size = new System.Drawing.Size(563, 33);
             this.tB.TabIndex = 2;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(883, 608);
+            this.ClientSize = new System.Drawing.Size(662, 579);
             this.Controls.Add(this.tB);
             this.Controls.Add(this.listBoxForum);
             this.Controls.Add(this.buttonOtpravka);
-            this.Font = new System.Drawing.Font("Karnac One", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Font = new System.Drawing.Font("Marta Decor Two", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "Form2";
-            this.Text = "?";
             this.ResumeLayout(false);
             this.PerformLayout();
 
